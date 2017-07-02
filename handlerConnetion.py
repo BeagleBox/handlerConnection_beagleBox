0dd5ea8e1dabc35238aea77ec404ff56fc8196f6
joaohenriquepda <joaohenrique.p.almeida@gmail.com>
2017-07-02 13:26:41 -0300

    add raspberry commands and sensors

--- handlerConnetion.py
+++ handlerConnetion.py
@@ -1,9 +1,28 @@
+# No caso eu to mandando uma mensagem "aviso" e "Desolocamento"
+# pra identificar o tipo de dado que eu estou enviando,
+# a questão do nivel de bateria, e da balança a agente vai pegar da propria placa,
+# ou seja, vai tá disponível a todo momento, não vai vir do arduino
+
 import json
 import pprint
 import sys
 from SendSMS import SendSMS
 from Delivery import Delivery
 from websocket import create_connection
+import RPi.GPIO as GPIO
+import time
+import serial
+
+#Configura a serial e a velocidade de transmissao
+ser = serial.Serial("/dev/ttyAMA0", 115200)
+GPIO.setmode(GPIO.BOARD)
+#Entrada do sinal da balança
+GPIO.setup(10,GPIO,IN)
+#Entrada do sinal do nivel de bateria
+GPIO.setup(12,GPIO,IN)
+
+ser.write(identificadorDeCaminho)
+
 
 
 ws = create_connection("ws://localhost:3000/cable")
@@ -14,6 +33,19 @@ def main():
     result = json.loads(result)
     print result
     print result.get("type")
+
+    balanca = GPIO.input(10)
+    nivelBateria = GPIO.input(12)
+    resposta = ser.readline()
+        if (resposta == "Aviso") :
+                aviso = ser.readline() #INICIO, FIM, OBSTRUIDO
+        if (resposta == "Deslocamento") :
+                deslocamento = ser.readline() #NUMERO DE VEZES QUE DESLOCOU NO EIXO X.
+        if (trancar == 1) :
+                ser.write(trancar)
+
+        time.sleep(0.5)
+
 
 if __name__ == "__main__":
     while 1:
